0a145b2051be5386fd9f17540e4ead21e2028dc7
Glauco Silva <glaucodasilva@live.com>
2023-01-30 12:29:35 +0000

    Adicionados novos cenários de teste para produtos  On branch v1.3 	modified:   cypress/e2e/produtos.cy.js 	modified:   cypress/fixtures/produtos.json 	modified:   cypress/support/commands.js

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -18,17 +18,53 @@ describe('teste da API https://serverest.dev/ - Produtos', () => {
     });
 
     it('cadastrar produto', () => {
-        let novoProd = {}
-        cy.geraStringAleatoria(5).then(retorno => {novoProd.nome = retorno; novoProd.preco = 255})
-        cy.geraStringAleatoria(10).then(retorno => {novoProd.descricao = retorno; novoProd.quantidade = 5})
-        
-        cy.listarUsuarios().then(listaDeUsuarios => {
-            cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
-                cy.cadastrarProdutos(novoProd, responseToken.body.authorization).then(response => {
-                    expect(response.body._id).to.exist
-                    novoProd._id = response.body._id
-                    produtos.push(novoProd)
-                    cy.writeFile('./cypress/fixtures/produtos.json', produtos)
+        cy.geraProd().then(novoProd => {
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.cadastrarProdutos(novoProd, responseToken.body.authorization).then(response => {
+                        expect(response.body._id).to.exist
+                        novoProd._id = response.body._id
+                        produtos.push(novoProd)
+                        cy.writeFile('./cypress/fixtures/produtos.json', produtos)
+                    })
+                })
+            })
+        })
+    });
+
+    it('ao tentar cadastrar um produto com mesmo nome recebemos a mensagem "Já existe produto com esse nome"', () => {
+        cy.listarProdutos().then(response => {
+            let novoProd = response.body.produtos[0]
+            delete novoProd._id
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.cadastrarProdutos(novoProd, responseToken.body.authorization).then(response => {
+                        expect(response.status).to.equal(400)
+                    })
+                })
+            })
+        })
+    });
+
+    it('ao tentar cadastrar um produto sem um token válido retornará status 401', () => {
+        cy.geraProd().then(novoProd => {
+            cy.cadastrarProdutos(novoProd, "semToken").then(response => {
+                expect(response.status).to.equal(401)
+                expect(response.body.message).to.contain("Token de acesso ausente, inválido, expirado ou usuário do token não existe mais")
+            })
+        })
+    });
+
+    it('ao tentar cadastrar um produto com o token de um usuário não administrador retornará status 403', () => {
+        cy.geraProd().then(novoProd => {
+            cy.cadastraUsuario(undefined,"false").then(() => {
+                cy.listarUsuarios().then(listaDeUsuarios => {
+                    cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'false')[0]).then(responseToken => {
+                        cy.cadastrarProdutos(novoProd, responseToken.body.authorization).then(response => {
+                            expect(response.status).to.equal(403)
+                            expect(response.body.message).to.contain("Rota exclusiva para administradores")
+                        })
+                    })
                 })
             })
         })
@@ -40,39 +76,148 @@ describe('teste da API https://serverest.dev/ - Produtos', () => {
         })
     });
 
+    it('ao buscar um produto inexistente pelo id deve retornar status 400', () => {
+        let prodFake = {}
+        prodFake._id = 'idFake'
+        cy.listarProdutos(prodFake).then(response => {
+            expect(response.status).to.equal(400)
+            expect(response.body.message).to.contain("Produto não encontrado")
+        })
+    });
+
     it('atualizar um produto cadastrado', () => {
-        cy.listarUsuarios().then(listaDeUsuarios => {
-            cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
-                cy.updateProduto(produtos.filter((prod) => prod._id != undefined)[0], responseToken.body.authorization).then( response => {
-                    expect(response.body.message).to.have.contain("Registro alterado com sucesso")
+        cy.listarProdutos(listaProdutos => {
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.updateProduto(listaProdutos.body.produtos[0], responseToken.body.authorization).then( response => {
+                        expect(response.body.message).to.have.contain("Registro alterado com sucesso")
+                    })
                 })
             })
         })
     });
 
-    it('excluir um item da lista produtos.json', () => {
-        let sair = false
-        let iExcluir = []
-        let excluir = produtos.filter((prod, indice) => {
-            if (prod._id != undefined) {
-                iExcluir.push(indice)
-                return true
-            }
-        })
+    it('ao tentar atualizar um produto inexistente o sistema irá cadastrá-lo', () => {
+        cy.geraProd().then(novoProd => {
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.updateProduto(novoProd, responseToken.body.authorization).then( response => {
+                        expect(response.status).to.equal(201)
+                        expect(response.body.message).to.equal("Cadastro realizado com sucesso")
+                    })
+                })
+            })
+        }) 
+    });
 
-        cy.listarUsuarios().then(listaDeUsuarios => {
-            cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
-                for (let i = 0; i < excluir.length; i++) {
-                    if (sair == true) break
-                    cy.excluirProduto(excluir[i], responseToken.body.authorization).then(response => {
-                        if(response.body.message == 'Registro excluído com sucesso') {
-                            sair = true
-                            produtos.splice(iExcluir[i], 1)
-                            cy.writeFile('./cypress/fixtures/produtos.json', produtos)
-                        } 
-                    }) 
-                }
+    it('ao tentar atualizar um produto com o nome de outro já existente retorna status 400', () => {
+        cy.listarProdutos().then(listaProds => {
+            let atualizaProd = listaProds.body.produtos[0]
+            atualizaProd.nome = listaProds.body.produtos[1].nome
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.updateProduto(atualizaProd, responseToken.body.authorization).then( response => {
+                        expect(response.status).to.equal(400)
+                        expect(response.body.message).to.equal("Já existe produto com esse nome")
+                    })
+                })
             })
         })
+    });
+
+    it('ao tentar atualizar um produto com um token ausente, inválido ou expirado deve retornar status 401', () => {
+        cy.listarProdutos().then(listarProdutos => {
+            const prods = listarProdutos.body.produtos
+            cy.updateProduto(prods[0], 'tokenInvalido').then( response => {
+                expect(response.status).to.equal(401)
+                expect(response.body.message).to.equal("Token de acesso ausente, inválido, expirado ou usuário do token não existe mais")
+            })
+        })
+    });
+
+    it('ao tentar atualizar um produto com um usuário não administrador retorna erro 403', () => {
+        cy.cadastraUsuario(undefined, 'false').then(respNovoUser => {
+            let novoUser = {}
+            novoUser._id = respNovoUser.body._id
+            cy.listarProdutos().then(listaProdutos => {
+                cy.listarUsuarios(novoUser).then(user => {
+                    cy.login(user.body).then(responseToken => {
+                        cy.updateProduto(listaProdutos.body.produtos[0], responseToken.body.authorization).then( response => {
+                            expect(response.status).to.equal(403)
+                            expect(response.body.message).to.equal("Rota exclusiva para administradores")
+                        })
+                    })
+                })
+            })
+        })
+    });
+
+    it('excluir um item que não esta em um carrinho de compras', () => {
+        cy.listaProdSemCar().then(listaProdSemCar => {
+            let excluir = {}
+            excluir._id =  listaProdSemCar[0]
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.excluirProduto(excluir, responseToken.body.authorization).then(response => {
+                        expect(response.body.message).to.contain('Registro excluído com sucesso') 
+                    }) 
+                })
+            })
+        }) 
+    });
+
+    it('ao tentar excluir um item inexistente deve retornar mensagem "Nenhum registro excluído"', () => {
+        cy.listaProdSemCar().then(listaProdSemCar => {
+            let excluir = {}
+            excluir._id =  'idInvalido'
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.excluirProduto(excluir, responseToken.body.authorization).then(response => {
+                        expect(response.body.message).to.contain('Nenhum registro excluído') 
+                    }) 
+                })
+            })
+        }) 
+    });
+
+    it('ao tentar excluir um produto que esta no carrinho deve retornar status 400', () => {
+        cy.listaProdComCar().then(listaProdComCar => {
+            let excluir = {}
+            excluir._id =  listaProdComCar[0]
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'true')[0]).then(responseToken => {
+                    cy.excluirProduto(excluir, responseToken.body.authorization).then(response => {
+                        expect(response.status).to.equal(400)
+                        expect(response.body.message).to.contain("Não é permitido excluir produto que faz parte de carrinho")
+                    }) 
+                })
+            })
+        }) 
+    });
+
+    it('ao tentar excluir um produto com token ausente, inválido ou expirado deve retornar status 401', () => {
+        cy.listaProdSemCar().then(listaProdSemCar => {
+            let excluir = {}
+            excluir._id =  listaProdSemCar[0]
+            cy.excluirProduto(excluir, 'tokenInvalido').then(response => {
+                expect(response.status).to.equal(401)
+                expect(response.body.message).to.equal("Token de acesso ausente, inválido, expirado ou usuário do token não existe mais")
+            })
+        }) 
+    });
+
+    it('ao tentar excluir um produto com um user não administrador deve retornar status 403', () => {
+        cy.listaProdSemCar().then(listaProdSemCar => {
+            let excluir = {}
+            excluir._id =  listaProdSemCar[0]
+            cy.listarUsuarios().then(listaDeUsuarios => {
+                cy.login(listaDeUsuarios.body.usuarios.filter((user) => user.administrador == 'false')[0]).then(responseToken => {
+                    cy.excluirProduto(excluir, responseToken.body.authorization).then(response => {
+                        expect(response.status).to.equal(403)
+                        expect(response.body.message).to.equal("Rota exclusiva para administradores")
+                    }) 
+                })
+            })
+        }) 
     });
 });
